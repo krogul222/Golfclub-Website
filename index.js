acf1fb42a162e4da3be15707e3fcf73d830729f5
Krzysztof Krogulski <krzysztof.krogulski@nexperia.com>
2017-07-26 19:31:05 +0100

    Socketio and mongojs added

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
+var mongojs = require('mongojs');
 var express = require('express');
+var db = mongojs('mongodb://golf:nexperia@ds123193.mlab.com:23193/nexperiagolfsociety', ['account']);
 
 var app = express();
+var server = require('http').Server(app);
 
 app.use(express.static('public'));
 
-var listener = app.listen(process.env.PORT || 3000, function(){
-  console.log('listening on', listener.address().port);
+var listener = server.listen(process.env.PORT || 3000, function(){ 
+     console.log("Listening on port", listener.address().port); 
+ }); 
+
+
+const SOCKET_LIST = {};
+
+let io = require('socket.io')(server,{}); 
+io.sockets.on('connection', function(socket){ 
+    console.log("Socket connection"); 
+    socket.id = Math.random(); 
+    SOCKET_LIST[socket.id] = socket; 
+
 });
