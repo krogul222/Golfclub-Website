4ee457e6bc7f7daa1975082af73812e45fa32757
Krzysztof Krogulski <krzysztof.krogulski@nexperia.com>
2017-08-01 17:07:17 +0100

    Edit panel upgraded

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var express = require('express');
 //var db = mongojs('mongodb://golf:nexperia@ds123193.mlab.com:23193/nexperiagolfsociety', ['account']);
 
 var db = mongojs('localhost:27017/golf', ['account']);
-/*
-db.account.remove();
 
+//db.account.remove();
+/*
 db.account.insert({username:"johnhart", name: "John Hart", password: "johnhart", email: "johnymike@hotmail.com", phone: "07930980836", committee: "true", position: "President", handicapPlaying: "14", handicapExact: "14.2"});
 
 db.account.insert({username:"garynorton", name: "Gary Norton", password: "garynorton", email: "", phone: "", committee: "true", position: "Treasurer", handicapPlaying: "22", handicapExact: "22" });
@@ -55,7 +55,19 @@ let isUserAdmin = function(data, cb){
      }); 
  } 
 
-
+function isUsernameTaken(user, cb){
+    
+    if(db.account.find({username:user}, function(err, res){
+        console.log(user);
+        if(res.length>0){
+            cb(true);
+        }
+        else{
+          cb(false);  
+        }
+        
+    }));  
+}
 
 const SOCKET_LIST = {};
 
@@ -86,9 +98,32 @@ io.sockets.on('connection', function(socket){
      });
     
     socket.on('addMember',function(data){
-        db.account.insert(data);
-        socket.emit('memberAdded',data);
+
+        isUsernameTaken(data["username"], function(res){
+           if(res){
+             socket.emit('memberNotAdded',{});   
+           } else{
+             db.account.insert(data);
+             socket.emit('memberAdded',data);  
+           } 
+        });
     });
+    
+     socket.on('editMember',function(data){
+         console.log("Edit data: "+data.username+" "+ data.name+" "+ data.handicapExact);
+         
+         db.account.update({username: data.username},{$set:{username: data.username, name: data.name, handicapExact: data.handicapExact}});
+         
+         socket.emit('memberEdited',data);
+     });
+    
+     socket.on('deleteMember',function(data){
+         console.log("Delete data: "+data.username+" "+ data.name+" "+ data.handicapExact);
+         
+         db.account.remove({username: data.username});
+         
+         socket.emit('memberDeleted',data);
+     });    
     
     socket.on('signIn',function(data){
         isValidPassword(data, function(res){ 
