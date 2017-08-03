994892a5dcdeba0032449b8fd17d8454917004d8
Krzysztof Krogulski <krzysztof.krogulski@nexperia.com>
2017-08-03 16:37:27 +0100

    small js refactorisation

--- index.js
+++ index.js
@@ -28,113 +28,97 @@ var server = require('http').Server(app);
 app.use(express.static('public'));
 
 var listener = server.listen(process.env.PORT || 2000, function(){ 
-     console.log("Listening on port", listener.address().port); 
- }); 
+    console.log("Listening on port", listener.address().port); 
+}); 
 
 let isValidPassword = function(data, cb){ 
-     db.account.find({username:data.username, password:data.password}, function(err, res){ 
-        console.log(data.username + ' ' + data.password);
-         if(res.length > 0){ 
-            console.log("success");
+    db.account.find({username:data.username, password:data.password}, function(err, res){ 
+        if(res.length > 0){ 
             cb(true); 
-         } else{ 
-            console.log("failure");
+        } else{ 
             cb(false); 
-         } 
-     }); 
- } 
+        } 
+    }); 
+} 
  
 let isUserAdmin = function(data, cb){ 
-     db.account.find({username:data.username, admin: "true"}, function(err, res){ 
-        console.log(data.username + ' ' + data.password);
-         if(res.length > 0){ 
-            console.log("success");
+    db.account.find({username:data.username, admin: "true"}, function(err, res){ 
+        if(res.length > 0){ 
             cb(true); 
-         } else{ 
-            console.log("failure");
+        } else{ 
             cb(false); 
-         } 
-     }); 
- } 
+        } 
+    }); 
+} 
 
 function isUsernameTaken(user, cb){
-    
     if(db.account.find({username:user}, function(err, res){
-        console.log(user);
         if(res.length>0){
             cb(true);
-        }
-        else{
-          cb(false);  
-        }
-        
+        } else{
+            cb(false);  
+        }    
     }));  
 }
 
 const SOCKET_LIST = {};
 
 let io = require('socket.io')(server,{}); 
+
 io.sockets.on('connection', function(socket){ 
     console.log("Socket connection"); 
+    
     socket.id = Math.random(); 
     SOCKET_LIST[socket.id] = socket; 
 
-    socket.on('dataRequest',function(data){ 
-         switch(data){
-             case "committee":
-                 console.log("committee request sent");
-                  db.account.find({committee: "true"}, function(err, res){
+    socket.on('dataRequest', function(data){ 
+        switch(data){
+            case "committee":
+                db.account.find({committee: "true"}, function(err, res){
                     socket.emit('committeeData',res);
-                      console.log("committee data sent");
-                  });
-                 break;
-                 
-             case "membershiplisting":
-                 console.log("membershiplisting request sent");
-                  db.account.find({account: "member"}, function(err, res){
+                });
+                break; 
+            case "membershiplisting":
+                db.account.find({account: "member"}, function(err, res){
                     socket.emit('membershiplistingData',res);
-                      console.log("membershiplisting data sent");
-                  });
-                 break;
-            }
-     });
+                });
+                break;
+        }
+    });
     
-    socket.on('addMember',function(data){
-
+    socket.on('addMember', function(data){
         isUsernameTaken(data["username"], function(res){
-           if(res){
-               console.log("taken");
-             socket.emit('usernametaken',{});   
-           } else{
-             data["account"] = "member";
-             db.account.insert(data);
-             socket.emit('memberAdded',data);  
-           } 
+            if(res){
+                socket.emit('usernametaken',{});   
+            } else{
+                data["account"] = "member";
+                db.account.insert(data);
+                socket.emit('memberAdded',data);  
+            }
         });
     });
     
-    socket.on('changePassword',function(data){
+    socket.on('changePassword', function(data){
         db.account.update({username: data.username},{$set:{password: data.password}});
         socket.emit('passwordChanged',data);
     });
 
-    socket.on('changeUserDetails',function(data){
-        
+    socket.on('changeUserDetails', function(data){
         if(data.oldusername === data.username){
             db.account.update({username: data.oldusername},{$set:{username: data.username, name: data.name, email: data.email, phone: data.phone}});
             socket.emit('userDetailsChanged',data);
+        } else{
+            isUsernameTaken(data.username, function(res){
+            if(res){
+                socket.emit('usernametaken',{});   
             } else{
-        isUsernameTaken(data.username, function(res){
-           if(res){
-             socket.emit('usernametaken',{});   
-           } else{
-             db.account.update({username: data.oldusername},{$set:{username: data.username, name: data.name}});
-             socket.emit('userDetailsChanged',data);
-           }});
-           }
+                db.account.update({username: data.oldusername},{$set:{username: data.username, name: data.name}});
+                socket.emit('userDetailsChanged',data);
+            }});
+        }
     });    
     
-    socket.on('checkCurrentPassword',function(data){
+    socket.on('checkCurrentPassword', function(data){
         isValidPassword(data, function(res){
             if(res){ 
                 socket.emit('passwordCorrect',data);
@@ -142,42 +126,31 @@ io.sockets.on('connection', function(socket){
                 socket.emit('passwordIncorrect',data);
             }
         });
-        
     });
     
-    socket.on('editMember',function(data){
-         console.log("Edit data: "+data.username+" "+ data.name+" "+ data.handicapExact);
-         
+    socket.on('editMember', function(data){
          db.account.update({username: data.username},{$set:{username: data.username, name: data.name, handicapExact: data.handicapExact}});
-         
          socket.emit('memberEdited',data);
      });
     
-     socket.on('deleteMember',function(data){
-         console.log("Delete data: "+data.username+" "+ data.name+" "+ data.handicapExact);
-         
-         db.account.remove({username: data.username});
-         
-         socket.emit('memberDeleted',data);
-     });    
+    socket.on('deleteMember', function(data){
+        db.account.remove({username: data.username}); 
+        socket.emit('memberDeleted',data);
+    });    
     
-    socket.on('signIn',function(data){
+    socket.on('signIn', function(data){
         isValidPassword(data, function(res){ 
             if(res){ 
-                 isUserAdmin(data, function(res2){
-                     if(res2){
-                         socket.emit('signInResponse',{success: true, username: data["username"], admin: "true"}); 
-                     } else{
+                isUserAdmin(data, function(res2){
+                    if(res2){
+                        socket.emit('signInResponse',{success: true, username: data["username"], admin: "true"}); 
+                    } else{
                         socket.emit('signInResponse',{success: true, username: data["username"], admin: "false"});  
-                     }
-                 });
-                 
+                    }
+                }); 
             } else{ 
-                 socket.emit('signInResponse',{success: false}); 
+                socket.emit('signInResponse',{success: false}); 
             } 
-         }); 
-
+        }); 
     });
-
-    
 });
