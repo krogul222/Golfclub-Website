ab53b113b78f818f4ae70074079f520814091288
Krzysztof Krogulski <krzysztof.krogulski@nexperia.com>
2017-08-25 03:50:14 +0100

    edit competition panel added in basic version

--- index.js
+++ index.js
@@ -23,17 +23,17 @@ db.account.insert({username:"admin", name: "Krzysztof Krogulski", password: "123
 
 //db.event.remove();
 /*
-db.event.insert({name: "August Society Day", course: "Dukinfield Golf Club", postcode: "SK165GF", website: "http://www.dukinfieldgolfclub.co.uk/", price: "15", included: "golf", maxplayers: "20", firstteatime: "09:30", day: "07", month: "08", year: "2017", organiser: "Stephen Percy", notes: "" , id: "1"});
+db.event.insert({name: "August Society Day", course: "Dukinfield Golf Club", postcode: "SK165GF", website: "http://www.dukinfieldgolfclub.co.uk/", price: "15", included: "golf", maxplayers: "20", firstteetime: "09:30", day: "07", month: "08", year: "2017", organiser: "Stephen Percy", notes: "" , id: "1"});
 
-db.event.insert({name: "1st game of the year", course: "North Manchester", postcode: "", website: "", price: "18.50", included: "bacon butty and coffee", maxplayers: "0", firstteatime: "09:30", day: "04", month: "03", year: "2016", organiser: "Gary Norton", notes: "", id: "2" });
+db.event.insert({name: "1st game of the year", course: "North Manchester", postcode: "", website: "", price: "18.50", included: "bacon butty and coffee", maxplayers: "0", firstteetime: "09:30", day: "04", month: "03", year: "2016", organiser: "Gary Norton", notes: "", id: "2" });
 
-db.event.insert({name: "Player of the year game 2", course: "Cavendish GC", postcode: "", website: "", price: "19", included: "golf", maxplayers: "20", firstteatime: "11:00", day: "28", month: "04", year: "2016", organiser: "Paul Craney Barnie", notes: "come along and play one of the best courses in our area for a reasonable price, £10 deposit to secure place unfortunately on this occasion full payment needs to be paid by Monday 18th April, 20 places available,cheers guys . Paul C.B", id: "3" });
+db.event.insert({name: "Player of the year game 2", course: "Cavendish GC", postcode: "", website: "", price: "19", included: "golf", maxplayers: "20", firstteetime: "11:00", day: "28", month: "04", year: "2016", organiser: "Paul Craney Barnie", notes: "come along and play one of the best courses in our area for a reasonable price, £10 deposit to secure place unfortunately on this occasion full payment needs to be paid by Monday 18th April, 20 places available,cheers guys . Paul C.B", id: "3" });
 
-db.event.insert({name: "Marple", course: "Marple", postcode: "", website: "", price: "16.50", included: "golf, fish and chips", maxplayers: "24", firstteatime: "09:00", day: "23", month: "05", year: "2016", organiser: "Graham Stafford", notes: "", id: "4" });
+db.event.insert({name: "Marple", course: "Marple", postcode: "", website: "", price: "16.50", included: "golf, fish and chips", maxplayers: "24", firstteetime: "09:00", day: "23", month: "05", year: "2016", organiser: "Graham Stafford", notes: "", id: "4" });
 
-db.event.insert({name: "Cavendish - the return", course: "Cavendish", postcode: "", website: "", price: "18", included: "golf", maxplayers: "16", firstteatime: "08:10", day: "13", month: "09", year: "2016", organiser: "John Hart", notes: "Back to Cavendish for our half price game after April's game there was hit with snow! Only 16 places so names down quick.", id: "5" });
+db.event.insert({name: "Cavendish - the return", course: "Cavendish", postcode: "", website: "", price: "18", included: "golf", maxplayers: "16", firstteetime: "08:10", day: "13", month: "09", year: "2016", organiser: "John Hart", notes: "Back to Cavendish for our half price game after April's game there was hit with snow! Only 16 places so names down quick.", id: "5" });
 
-db.event.insert({name: "STAMFORD", course: "Stamford G.C", postcode: "", website: "", price: "15", included: "golf", maxplayers: "16", firstteatime: "09:45", day: "05", month: "10", year: "2016", organiser: "John Hart", notes: "", id: "6" });
+db.event.insert({name: "STAMFORD", course: "Stamford G.C", postcode: "", website: "", price: "15", included: "golf", maxplayers: "16", firstteetime: "09:45", day: "05", month: "10", year: "2016", organiser: "John Hart", notes: "", id: "6" });
 */
 //db.event.insert({})
 
@@ -147,6 +147,7 @@ io.sockets.on('connection', function(socket){   // runs if client connected to t
                     data["results"][i]["month"] = data["month"];
                     data["results"][i]["day"] = data["day"];
                     data["results"][i]["id"] = data["id"];
+                    console.log("Mamber "+data["results"][i]["member"]);
                     db.competition.insert(data["results"][i]); 
                 }
                 //db.event.insert(data);            // add event to database
@@ -164,7 +165,7 @@ io.sockets.on('connection', function(socket){   // runs if client connected to t
     });
 
     socket.on('editSocietyEvent', function(data){     
-         db.event.update({id: data.id},{$set:{name: data.name, course: data.course, postcode: data.postcode, website: data.website, price: data.price, included: data.included, maxplayers: data.maxplayers, firstteatime: data.firstteatime, day: data.day, month: data.month, year: data.year, organiser: data.organiser, notes: data.notes}});  
+         db.event.update({id: data.id},{$set:{name: data.name, course: data.course, postcode: data.postcode, website: data.website, price: data.price, included: data.included, maxplayers: data.maxplayers, firstteetime: data.firstteetime, day: data.day, month: data.month, year: data.year, organiser: data.organiser, notes: data.notes}});  
          socket.emit('eventEdited',data);  // sent response to client with the same data
      });    
     
@@ -185,6 +186,17 @@ io.sockets.on('connection', function(socket){   // runs if client connected to t
         socket.emit('competitionDeleted',data);  // sent response to client
     });     
     
+    socket.on('editMemberCompetitionResult', function(data){     // member data was sent to update existing member
+         db.competition.update({id: data.id, member: data.member},{$set:{handicap: data.handicap, netscore: data.netscore}});  // update netscore and handicap
+         console.log(data["member"]["name"]);
+         socket.emit('memberCompetitionResultEdited',data);  // sent response to client with the same data
+     });    
+
+    socket.on('deleteMemberCompetitionResult', function(data){   // request to delete specific username was sent
+        db.competition.remove({id: data.id, member: data.member}); 
+        console.log(data["member"]["name"]);
+        socket.emit('memberCompetitionResultDeleted',data);  // sent response to client
+    });     
     
     //requests from editmember panel    
     
