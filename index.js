3d276aab43c9dac096ccbb962d008ee424554301
Krzysztof Krogulski <krzysztof.krogulski@nexperia.com>
2017-08-25 06:00:52 +0100

    upgrade of competition edit panel

--- index.js
+++ index.js
@@ -6,34 +6,34 @@ var db = mongojs('localhost:27017/golf', ['account','event','competition']);    
 
 //db.account.remove();
 /*
-db.account.insert({username:"johnhart", name: "John Hart", password: "johnhart", email: "johnymike@hotmail.com", phone: "07930980836", committee: "true", position: "President", handicapExact: "14.2", account:"member", admin: "true", id:"1"});
+db.account.insert({username:"johnhart", name: "John Hart", password: "johnhart", email: "johnymike@hotmail.com", phone: "07930980836", committea: "true", position: "President", handicapExact: "14.2", account:"member", admin: "true", id:"1"});
 
-db.account.insert({username:"garynorton", name: "Gary Norton", password: "garynorton", email: "", phone: "", committee: "true", position: "Treasurer", handicapExact: "22", account:"member", id:"2"});
+db.account.insert({username:"garynorton", name: "Gary Norton", password: "garynorton", email: "", phone: "", committea: "true", position: "Treasurer", handicapExact: "22", account:"member", id:"2"});
 
-db.account.insert({username:"tonylyons", name: "Tony Lyons", password: "tonylyons", email: "", phone: "", committee: "true", position: "Handicap Chairman", handicapExact: "12.3", account:"member", id:"3"});
+db.account.insert({username:"tonylyons", name: "Tony Lyons", password: "tonylyons", email: "", phone: "", committea: "true", position: "Handicap Chairman", handicapExact: "12.3", account:"member", id:"3"});
 
-db.account.insert({username:"craighulton", name: "Craig Hulton", password: "craighulton", email: "", phone: "", committee: "true", position: "Capitan", handicapExact: "19.4", account:"member", id:"4"});
+db.account.insert({username:"craighulton", name: "Craig Hulton", password: "craighulton", email: "", phone: "", committea: "true", position: "Capitan", handicapExact: "19.4", account:"member", id:"4"});
 
-db.account.insert({username:"paulcraneybarnie", name: "Paul Craney Barnie", password: "paulcraneybarnie", email: "paulbarniedecor@sol.com", phone: "07402958921", committee: "true", position: "Handicap Secratary", handicapExact: "8", account:"member", id:"5"});
+db.account.insert({username:"paulcraneybarnie", name: "Paul Craney Barnie", password: "paulcraneybarnie", email: "paulbarniedecor@sol.com", phone: "07402958921", committea: "true", position: "Handicap Secratary", handicapExact: "8", account:"member", id:"5"});
 
-db.account.insert({username:"stephenpercy", name: "Stephen Percy", password: "stephenpercy", email: "steve.percy@nxp.com", phone: "07910751885", committee: "false", position: "", handicapExact: "18.3", admin: "true", account:"member", id:"6"});
+db.account.insert({username:"stephenpercy", name: "Stephen Percy", password: "stephenpercy", email: "steve.percy@nxp.com", phone: "07910751885", committea: "false", position: "", handicapExact: "18.3", admin: "true", account:"member", id:"6"});
 
 db.account.insert({username:"admin", name: "Krzysztof Krogulski", password: "123", admin: "true", account:"admin"});
 */
 
 //db.event.remove();
 /*
-db.event.insert({name: "August Society Day", course: "Dukinfield Golf Club", postcode: "SK165GF", website: "http://www.dukinfieldgolfclub.co.uk/", price: "15", included: "golf", maxplayers: "20", firstteetime: "09:30", day: "07", month: "08", year: "2017", organiser: "Stephen Percy", notes: "" , id: "1"});
+db.event.insert({name: "August Society Day", course: "Dukinfield Golf Club", postcode: "SK165GF", website: "http://www.dukinfieldgolfclub.co.uk/", price: "15", included: "golf", maxplayers: "20", firstteatime: "09:30", day: "07", month: "08", year: "2017", organiser: "Stephen Percy", notes: "" , id: "1"});
 
-db.event.insert({name: "1st game of the year", course: "North Manchester", postcode: "", website: "", price: "18.50", included: "bacon butty and coffee", maxplayers: "0", firstteetime: "09:30", day: "04", month: "03", year: "2016", organiser: "Gary Norton", notes: "", id: "2" });
+db.event.insert({name: "1st game of the year", course: "North Manchester", postcode: "", website: "", price: "18.50", included: "bacon butty and coffee", maxplayers: "0", firstteatime: "09:30", day: "04", month: "03", year: "2016", organiser: "Gary Norton", notes: "", id: "2" });
 
-db.event.insert({name: "Player of the year game 2", course: "Cavendish GC", postcode: "", website: "", price: "19", included: "golf", maxplayers: "20", firstteetime: "11:00", day: "28", month: "04", year: "2016", organiser: "Paul Craney Barnie", notes: "come along and play one of the best courses in our area for a reasonable price, £10 deposit to secure place unfortunately on this occasion full payment needs to be paid by Monday 18th April, 20 places available,cheers guys . Paul C.B", id: "3" });
+db.event.insert({name: "Player of the year game 2", course: "Cavendish GC", postcode: "", website: "", price: "19", included: "golf", maxplayers: "20", firstteatime: "11:00", day: "28", month: "04", year: "2016", organiser: "Paul Craney Barnie", notes: "come along and play one of the best courses in our area for a reasonable price, £10 deposit to secure place unfortunately on this occasion full payment needs to be paid by Monday 18th April, 20 places available,cheers guys . Paul C.B", id: "3" });
 
-db.event.insert({name: "Marple", course: "Marple", postcode: "", website: "", price: "16.50", included: "golf, fish and chips", maxplayers: "24", firstteetime: "09:00", day: "23", month: "05", year: "2016", organiser: "Graham Stafford", notes: "", id: "4" });
+db.event.insert({name: "Marple", course: "Marple", postcode: "", website: "", price: "16.50", included: "golf, fish and chips", maxplayers: "24", firstteatime: "09:00", day: "23", month: "05", year: "2016", organiser: "Graham Stafford", notes: "", id: "4" });
 
-db.event.insert({name: "Cavendish - the return", course: "Cavendish", postcode: "", website: "", price: "18", included: "golf", maxplayers: "16", firstteetime: "08:10", day: "13", month: "09", year: "2016", organiser: "John Hart", notes: "Back to Cavendish for our half price game after April's game there was hit with snow! Only 16 places so names down quick.", id: "5" });
+db.event.insert({name: "Cavendish - the return", course: "Cavendish", postcode: "", website: "", price: "18", included: "golf", maxplayers: "16", firstteatime: "08:10", day: "13", month: "09", year: "2016", organiser: "John Hart", notes: "Back to Cavendish for our half price game after April's game there was hit with snow! Only 16 places so names down quick.", id: "5" });
 
-db.event.insert({name: "STAMFORD", course: "Stamford G.C", postcode: "", website: "", price: "15", included: "golf", maxplayers: "16", firstteetime: "09:45", day: "05", month: "10", year: "2016", organiser: "John Hart", notes: "", id: "6" });
+db.event.insert({name: "STAMFORD", course: "Stamford G.C", postcode: "", website: "", price: "15", included: "golf", maxplayers: "16", firstteatime: "09:45", day: "05", month: "10", year: "2016", organiser: "John Hart", notes: "", id: "6" });
 */
 //db.event.insert({})
 
@@ -94,9 +94,9 @@ io.sockets.on('connection', function(socket){   // runs if client connected to t
 
     socket.on('dataRequest', function(data){    // data was requested from client
         switch(data){
-            case "committee":
-                db.account.find({committee: "true"}, function(err, res){
-                    socket.emit('committeeData',res);   // sent committe data to client
+            case "committea":
+                db.account.find({committea: "true"}, function(err, res){
+                    socket.emit('committeaData',res);   // sent committe data to client
                 });
                 break; 
             case "membershiplisting":
@@ -165,11 +165,11 @@ io.sockets.on('connection', function(socket){   // runs if client connected to t
     });
 
     socket.on('editSocietyEvent', function(data){     
-         db.event.update({id: data.id},{$set:{name: data.name, course: data.course, postcode: data.postcode, website: data.website, price: data.price, included: data.included, maxplayers: data.maxplayers, firstteetime: data.firstteetime, day: data.day, month: data.month, year: data.year, organiser: data.organiser, notes: data.notes}});  
+         db.event.update({id: data.id},{$set:{name: data.name, course: data.course, postcode: data.postcode, website: data.website, price: data.price, included: data.included, maxplayers: data.maxplayers, firstteatime: data.firstteatime, day: data.day, month: data.month, year: data.year, organiser: data.organiser, notes: data.notes}});  
          socket.emit('eventEdited',data);  // sent response to client with the same data
      });    
     
-    socket.on('deleteEvent', function(data){   // request to delete specific event was sent
+    socket.on('deleteavent', function(data){   // request to delete specific event was sent
         console.log("delete Event");
         db.event.remove({id: data.id}); 
         socket.emit('eventDeleted',data);  // sent response to client
